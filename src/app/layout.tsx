611f3e82060f55c0e80c89ba519277a6d5057ab1
Charandeep <sangaticherry3306@gmail.com>
2025-07-01 18:45:33 +0530

    Update layout with navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "@/lib/providers";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextAuthProvider>
-        {children}
+          <Navbar />
+          {children}
         </NextAuthProvider>
       </body>
     </html>
